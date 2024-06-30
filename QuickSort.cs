b72bab5503371e6b7ada7ddea69059df5e672a89
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2024-06-30 19:57:44 +0200

    Added Quick Sort Algorithms

---
+++ QuickSort.cs
@@ -0,0 +1,51 @@
+namespace QuickSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 34, 7, 23, 32, 5, 33 };
+            QuickSort(numbers, 0, numbers.Length - 1);
+
+            Console.WriteLine("Sorted numbers:");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivotIndex = Partition(array, left, right);
+                QuickSort(array, left, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, right);
+            }
+        }
+
+        private static int Partition(int[] array, int left, int right)
+        {
+            int pivot = array[right];
+            int i = left - 1;
+
+            for (int j = left; j < right; j++)
+            {
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, i + 1, right);
+            return i + 1;
+        }
+
+        private static void Swap(int[] array, int index1, int index2)
+        {
+            int temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+        }
+    }
+}
